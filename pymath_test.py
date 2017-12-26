1ddabad4dc72ec48d1407708fd2e2e20c8c2cd58
Andrew Kroshko <akroshko.public+devel@gmail.com>
2017-12-26 04:41:53 -0600

    SOme examples and a fix.

--- pymath_test.py
+++ pymath_test.py
@@ -3,5 +3,14 @@ import os,sys
 from pymath_common import *
 pymath_default_imports(globals(),locals())
 
+# TODO: allows multiple labels at start
+tic('fullthing')
+tic()
+tic('justpp')
 pp({'z':['1','2','3','4','5','6','7','8','9','10'],
     'y':['1','2','3','4','5','6','7','8','9','10']})
+toc('justpp')
+time.sleep(1)
+toc()
+time.sleep(1)
+toc('fullthing')
