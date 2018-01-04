4f20416abfe3e45ff837f35d38391b9012633d65
Andrew Kroshko <akroshko.public+devel@gmail.com>
2018-01-04 17:53:38 -0600

    Some nice functions and configurations.

--- pymath_common.py
+++ pymath_common.py
@@ -2,12 +2,12 @@
 """A standard set of utility and system administration functions."""
 # DO NOT EDIT DIRECTLY IF NOT IN python-stdlib-personal, THIS FILE IS ORIGINALLY FROM https://github.com/akroshko/python-stdlib-personal
 
-# Copyright (C) 2015-2017, Andrew Kroshko, all rights reserved.
+# Copyright (C) 2015-2018, Andrew Kroshko, all rights reserved.
 #
 # Author: Andrew Kroshko
 # Maintainer: Andrew Kroshko <akroshko@gmail.com>
 # Created: Wed Nov 15, 2017
-# Version: 20171224
+# Version: 20180103
 # URL: https://github.com/akroshko/python-stdlib-personal
 #
 # This program is free software: you can redistribute it and/or modify
@@ -83,7 +83,7 @@ def make_axis_symlog_y(ax,ythresh,xmin,xmax,ymin,ymax):
         ax.set_yscale('symlog',linthreshy=ythresh)
         if ymax > ythresh:
             print "Top symlog"
-            ax.hlines( ythresh,xmin,xmax,color='r')
+            ax.hlines(ythresh,xmin,xmax,color='r')
         if ymin < -ythresh:
             print "Bottom symlog"
             ax.hlines(-ythresh,xmin,xmax,color='r')
@@ -97,6 +97,7 @@ def pymath_default_imports(theglobals,thelocals):
     pymath_import_module(theglobals,thelocals,'pprint','pprint')
     pymath_import_module(theglobals,thelocals,'pprint','pp',submodule='pprint')
     # TODO: import a nice re matcher
+    pymath_import_module(theglobals,thelocals,'random','random')
     pymath_import_module(theglobals,thelocals,'re','re')
     pymath_import_module(theglobals,thelocals,'socket','socket')
     pymath_import_module(theglobals,thelocals,'datetime','datetime')
@@ -118,8 +119,29 @@ def pymath_default_imports(theglobals,thelocals):
     pymath_import_module(theglobals,thelocals,'numpy','np')
     pymath_import_module(theglobals,thelocals,'scipy','sp')
     pymath_import_module(theglobals,thelocals,'matplotlib','mpl')
+    # XXXX: this allows things to be done with no graphics
+    # TODO: have a nicer configuration that accomodates headless servers, but still allows graphics to pop up
+    theglobals['mpl'].use('Agg')
     pymath_import_module(theglobals,thelocals,'matplotlib','plt',submodule='pyplot')
-    # mpl.use('Agg')
+
     # from matplotlib.backends.backend_pdf import PdfPages
 
 # TODO: move pymath_default_imports_and_open_database in here
+
+################################################################################
+## some array utilities
+
+@All(globals())
+def compact_array_print(arr):
+    # TODO: string are immutable, this is not efficient...
+    thestring = ""
+    # TODO: do not use a for loop, make something that works well with various boolean things
+    # TODO: have something that gives relative magnitude (logarithmic too)
+    for i in xrange(arr.shape[1]):
+        for j in xrange(arr.shape[0]):
+            if arr[i,j] > 0.5:
+                thestring += '##'
+            else:
+                thestring += '..'
+        thestring += '\n'
+    return thestring
